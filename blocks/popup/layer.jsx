f93f15c2b6f43c39e0ae7a89ee18b670f37eb98a
Vladimir Varankin <nek.narqo+git@gmail.com>
2016-01-08 17:19:51 +0300

    add layer component to order popups

---
+++ blocks/popup/layer.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+const zIndexFactor = 1000;
+const visibleLayersZIndexes = {};
+
+class Layer extends Component {
+    constructor(...args) {
+        super(...args);
+        this.zIndex = null;
+    }
+
+    componentWillMount() {
+        if (this.props.visible) {
+            this.captureZIndex();
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.visible !== nextProps.visible)
+            if (nextProps.visible) {
+                this.captureZIndex();
+            } else {
+                this.releaseZIndex();
+            }
+    }
+
+    componentWillUnmount() {
+        this.releaseZIndex();
+    }
+
+    render() {
+        return this.props.children;
+    }
+
+    captureZIndex() {
+        const level = this.props.zIndexGroupLevel;
+        const zIndexes = visibleLayersZIndexes[level] ||
+            (visibleLayersZIndexes[level] = [(level + 1) * zIndexFactor]);
+        const prevZIndex = this.zIndex;
+
+        this.zIndex = zIndexes[zIndexes.push(zIndexes[zIndexes.length - 1] + 1) - 1];
+        if (this.zIndex !== prevZIndex) {
+            this.props.onOrderChange(this.zIndex);
+        }
+    }
+
+    releaseZIndex() {
+        const zIndexes = visibleLayersZIndexes[this.props.zIndexGroupLevel];
+        zIndexes.splice(zIndexes.indexOf(this.zIndex), 1);
+    }
+}
+
+Layer.defaultProps = {
+    visible: false,
+    zIndexGroupLevel: 0,
+    onOrderChange() {},
+};
+
+export default Layer;
