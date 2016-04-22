af67f16d087eaf1bc0197f1b732bef8121c7c6c4
Vladimir Varankin <nek.narqo+git@gmail.com>
2016-04-23 01:35:06 +0300

    link: fix disabled respond in pseudolink

--- blocks/Link/index.js
+++ blocks/Link/index.js
@@ -4,17 +4,19 @@ import Control from '../Control';
 import pressable from '../pressable';
 
 class Link extends Control {
-
     render() {
+        const { disabled } = this.props;
+
         if (this.props.url) {
+            const url = disabled ? null : this.props.url;
+
             return (
-                <a className={this.className()} {...this.getControlHandlers()} href={this.props.url} target={this.props.target} title={this.props.title}>
+                <a className={this.className()} {...this.getControlHandlers()} href={url} target={this.props.target} title={this.props.title}>
                     {this.props.children}
                 </a>
             );
-
         } else {
-            const tabIndex = this.props.disabled ? -1 : 0;
+            const tabIndex = disabled ? -1 : 0;
 
             return (
                 <span className={this.className()} {...this.getControlHandlers()} tabIndex={tabIndex} title={this.props.title}>
