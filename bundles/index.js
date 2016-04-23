17c77c67f6be8d77f6e277569fee544ea6727f66
Vladimir Varankin <nek.narqo+git@gmail.com>
2016-04-23 16:47:12 +0300

    reformat examples

--- bundles/index.js
+++ bundles/index.js
@@ -9,63 +9,83 @@ import RadioGroup from '../blocks/RadioGroup';
 import Checkbox from '../blocks/Checkbox';
 import CheckboxGroup from '../blocks/CheckboxGroup';
 
+class ButtonExample extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.onButtonClick = this.onButtonClick.bind(this);
+    }
+
+    render() {
+        return (
+            <div>
+                <Button name="btn1" theme="islands" size="s" onClick={this.onButtonClick}>Click me!</Button>
+                <span>&nbsp;</span>
+                <Button name="btn2-disabled" theme="islands" size="s" disabled onClick={this.onButtonClick}>Try click me!</Button>
+                <span>&nbsp;</span>
+                <Button theme="islands" size="s" type="link" url="#/https://yandex.ru" onClick={this.onButtonClick}>Yandex</Button>
+                <span>&nbsp;</span>
+                <Button theme="islands" size="s" type="link" url="#/https://yandex.ru" disabled onClick={this.onButtonClick}>Yandex</Button>
+            </div>
+        );
+    }
+
+    onButtonClick() {
+        console.log('button click!');
+    }
+}
+
+class LinkExample extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.onLinkClick = this.onLinkClick.bind(this);
+    }
+
+    render() {
+        return (
+            <div>
+                <p><Link theme="islands" size="s" url="#/https://yandex.ru">Yandex</Link></p>
+                <p>Pseudo <Link theme="islands" size="s" onClick={this.onLinkClick}>Yandex</Link></p>
+                <p>Disabled <Link theme="islands" size="s" url="#/https://yandex.ru" disabled onClick={this.onLinkClick}>Yandex</Link></p>
+            </div>
+        );
+    }
+
+    onLinkClick() {
+        console.log('link click!');
+    }
+}
+
 class RadioGroupExample extends React.Component {
-    constructor( props ) {
-        super( props );
+    constructor(props) {
+        super(props);
 
         this.state = {
             value: '30'
         };
 
-        this.onChange = this.onChange.bind( this );
+        this.onChange = this.onChange.bind(this);
+        this.onRadioCheck = this.onRadioCheck.bind(this);
+        this.onRadioClick = this.onRadioClick.bind(this);
+        this.onResetLinkClick = this.onResetLinkClick.bind(this);
     }
 
     render() {
         return (
             <div>
-                <RadioGroup name="phones" value={ this.state.value } theme="islands" size="l" type="button" onChange={ this.onChange }>
+                <RadioGroup name="phones" value={this.state.value} theme="islands" size="l" type="button" onChange={this.onChange}>
                     <Radio value="10">10</Radio>
-                    <Radio value="20" disabled>20</Radio>
-                    <Radio value="30" onClick={ (e) => console.log('CLICK') }>30</Radio>
+                    <Radio value="20" disabled onClick={this.onRadioClick}>20</Radio>
+                    <Radio value="30" onClick={this.onRadioClick}>30</Radio>
                     <Radio value="40">40</Radio>
                     <Radio value="50">50</Radio>
                 </RadioGroup>
+                &mdash;
+                <Link theme="islands" size="s" onClick={this.onResetLinkClick}>reset group</Link>
                 <div>Вы выбрали: <b>{ this.state.value }</b></div>
-            </div>
-        );
-    }
-
-    onChange( value ) {
-        this.setState( { value } );
-    }
-
-}
-
-class CheckboxGroupExample extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            value: ['30', '40']
-        };
-
-        this.onChange = this.onChange.bind(this);
-        this.onCheckboxCheck = this.onCheckboxCheck.bind(this);
-    }
-
-    render() {
-        return (
-            <div>
-                <CheckboxGroup name="phones" value={this.state.value} theme="islands" type="button" size="l" onChange={this.onChange}>
-                    <Checkbox value="10">10</Checkbox>
-                    <Checkbox value="20" disabled>20</Checkbox>
-                    <Checkbox value="30" focused>30</Checkbox>
-                    <Checkbox value="40">40</Checkbox>
-                    <Checkbox value="50">50</Checkbox>
-                </CheckboxGroup>
-                <div>Вы выбрали: <b>{this.state.value.join(', ')}</b></div>
                 <p>
-                    <Checkbox theme="islands" type="button" size="l" name="foo" value="40" onCheck={this.onCheckboxCheck}>Forty</Checkbox>
+                    <Radio theme="islands" type="button" size="l" name="foo" value="70" onCheck={this.onRadioCheck}>70</Radio>
                 </p>
             </div>
         );
@@ -75,9 +95,17 @@ class CheckboxGroupExample extends React.Component {
         this.setState({ value });
     }
 
-    onCheckboxCheck(checked, value) {
+    onRadioCheck(checked, value) {
         console.log(`checked: ${checked}`);
-        this.setState({ value: [value] });
+        //this.setState({ value });
+    }
+
+    onRadioClick() {
+        console.log('radio click!');
+    }
+
+    onResetLinkClick() {
+        this.setState({ value: '' });
     }
 }
 
@@ -114,6 +142,58 @@ class TextInputExample extends React.Component {
                 </p>
             </div>
         )
+    }
+}
+
+class CheckboxGroupExample extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: ['30', '40']
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onCheckboxCheck = this.onCheckboxCheck.bind(this);
+        this.onCheckboxClick = this.onCheckboxClick.bind(this);
+        this.onResetLinkClick = this.onResetLinkClick.bind(this);
+    }
+
+    render() {
+        return (
+            <div>
+                <CheckboxGroup name="phones" value={this.state.value} theme="islands" type="button" size="l" onChange={this.onChange}>
+                    <Checkbox value="10">10</Checkbox>
+                    <Checkbox value="20" disabled onClick={this.onCheckboxClick}>20</Checkbox>
+                    <Checkbox value="30" onClick={this.onCheckboxClick}>30</Checkbox>
+                    <Checkbox value="40">40</Checkbox>
+                    <Checkbox value="50">50</Checkbox>
+                </CheckboxGroup>
+                &mdash;
+                <Link theme="islands" size="s" onClick={this.onResetLinkClick}>reset group</Link>
+                <div>Вы выбрали: <b>{this.state.value.join(', ')}</b></div>
+                <p>
+                    <Checkbox theme="islands" type="button" size="l" name="foo" value="40" onCheck={this.onCheckboxCheck}>Forty</Checkbox>
+                </p>
+            </div>
+        );
+    }
+
+    onChange(value) {
+        this.setState({ value });
+    }
+
+    onCheckboxCheck(checked, value) {
+        console.log(`checked: ${checked}`);
+        this.setState({ value: [value] });
+    }
+
+    onCheckboxClick() {
+        console.log('checkbox click!');
+    }
+
+    onResetLinkClick() {
+        this.setState({ value: [] });
     }
 }
 
@@ -164,13 +244,8 @@ class Example extends React.Component {
     renderButton() {
         return (
             <div className="example">
-                <Button name="btn1" theme="islands" size="s" onClick={this.handleClick}>Click me!</Button>
-                <span>&nbsp;</span>
-                <Button name="btn2-disabled" theme="islands" size="s" disabled onClick={this.handleClick}>Try click me!</Button>
-                <span>&nbsp;</span>
-                <Button theme="islands" size="s" type="link" url="#/https://yandex.ru" onClick={this.handleClick}>Yandex</Button>
-                <span>&nbsp;</span>
-                <Button theme="islands" size="s" type="link" url="#/https://yandex.ru" disabled  onClick={this.handleClick}>Yandex</Button>
+                <h1>Button Example</h1>
+                <ButtonExample />
             </div>
         );
     }
@@ -178,8 +253,8 @@ class Example extends React.Component {
     renderLink() {
         return (
             <div className="example">
-                <p>Link: <Link theme="islands" size="s" url="#/https://yandex.ru">Yandex</Link></p>
-                <p>Pseudo link: <Link theme="islands" size="s" onClick={this.handleClick}>Yandex</Link></p>
+                <h1>Link Example</h1>
+                <LinkExample />
             </div>
         )
     }
@@ -187,6 +262,7 @@ class Example extends React.Component {
     renderTextInput() {
         return (
             <div className="example">
+                <h1>TextInput Example</h1>
                 <TextInputExample />
             </div>
         );
@@ -220,13 +296,9 @@ class Example extends React.Component {
 
     renderRadioGroup() {
         return (
-            <div>
-                <div className="example">
-                    <RadioGroupExample/>
-                </div>
-                <div className="example">
-                    <Radio theme="islands" type="button" size="l" name="foo" value="70">70</Radio>
-                </div>
+            <div className="example">
+                <h1>RadioGroup Example</h1>
+                <RadioGroupExample/>
             </div>
         );
     }
@@ -234,6 +306,7 @@ class Example extends React.Component {
     renderCheckboxGroup() {
         return (
             <div className="example">
+                <h1>CheckboxGroup Example</h1>
                 <CheckboxGroupExample/>
             </div>
         );
