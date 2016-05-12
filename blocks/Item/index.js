a73e87325fb944283d4bee85a9963b8c838726b1
Vladimir Varankin <nek.narqo+git@gmail.com>
2016-05-13 01:56:50 +0300

    group, item: declare propTypes

--- blocks/Item/index.js
+++ blocks/Item/index.js
@@ -1,6 +1,12 @@
+import React from 'react';
 import Component from '../Component';
 
-class Item extends Component {
-}
+class Item extends Component {}
+
+Item.propTypes = {
+    disabled: React.PropTypes.bool,
+    value: React.PropTypes.any,
+    checkedText: React.PropTypes.any,
+};
 
 export default Item;
